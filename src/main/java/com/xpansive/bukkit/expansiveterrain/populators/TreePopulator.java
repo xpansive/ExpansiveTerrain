0079450af8b50efcd8960d1d9b4cd6eedfc1e21d
xpansive <xpansive@live.ca>
2011-07-24 18:41:26 -0600

    Varying amounts/types of trees/wild grass depending on biome

--- src/main/java/com/xpansive/bukkit/expansiveterrain/populators/TreePopulator.java
+++ src/main/java/com/xpansive/bukkit/expansiveterrain/populators/TreePopulator.java
@@ -19,30 +19,95 @@ public class TreePopulator extends BlockPopulator {
 		int z = (source.getZ() << 4) + random.nextInt(16);
 		int y = world.getHighestBlockYAt(x, z);
 
-		if (random.nextInt(128) - y <= 64) { // less chance of trees higher up
-			for (int i = 0; i < SHRUB_NUM; i++) {
-				int cx = x + (-SHRUB_RADIUS + (int) (random.nextDouble() * ((SHRUB_RADIUS - -SHRUB_RADIUS) + 1)));
-				int cz = z + (-SHRUB_RADIUS + (int) (random.nextDouble() * ((SHRUB_RADIUS - -SHRUB_RADIUS) + 1)));
+		int birchChance = 0, treeChance = 0, bigTreeChance = 0, redwoodChance = 0, tallRedwoodChance = 0, treeNum = 0;
 
-				Block b = world.getBlockAt(cx, world.getHighestBlockYAt(cx, cz), cz);
-				if (cx != x && cz != z // not where the tree goes
-						&& b.getRelative(0, -1, 0).getType() == Material.GRASS) {
-					b.setType(Material.LONG_GRASS);
-
-					b.setData((byte) (random.nextInt(100) < 3 ? 0 : 2));
-				}
+		switch (world.getBlockAt(x, y, z).getBiome()) {
+		case RAINFOREST:
+			birchChance = 20;
+			treeChance = 40;
+			bigTreeChance = 100;
+			treeNum = 4;
+			break;
+		case FOREST:
+		case SEASONAL_FOREST:
+			birchChance = 30;
+			treeChance = 60;
+			bigTreeChance = 100;
+			treeNum = 4;
+			break;
+		case SWAMPLAND:
+			birchChance = 10;
+			treeChance = 20;
+			bigTreeChance = 25;
+			treeNum = 1;
+			break;
+		case SAVANNA:
+			birchChance = 5;
+			treeChance = 10;
+			treeNum = 1;
+			break;
+		case SHRUBLAND:
+			birchChance = 5;
+			treeChance = 15;
+			treeNum = 1;
+			break;
+		case TAIGA:
+			redwoodChance = 50;
+			tallRedwoodChance = 100;
+			treeNum = 2;
+			break;
+		case PLAINS:
+			birchChance = 3;
+			treeChance = 6;
+			treeNum = 1;
+			break;
+		case TUNDRA:
+			redwoodChance = 5;
+			tallRedwoodChance = 10;
+			treeNum = 1;
+			break;
+		}
+		treeNum = random.nextInt(treeNum + 1) + 1;
+		int shrubNum = random.nextInt(SHRUB_NUM + 1);
+		for (int j = 0; j < treeNum; j++) {
+			Location[] shrubLoc = new Location[shrubNum];
+			x = (source.getX() << 4) + random.nextInt(16);
+			z = (source.getZ() << 4) + random.nextInt(16);
+			y = world.getHighestBlockYAt(x, z);
+			for (int i = 0; i < shrubNum; i++) {
+				Location l = new Location(world, 0, 0, 0);
+				l.setX(x + (-SHRUB_RADIUS + (int) (random.nextDouble() * ((SHRUB_RADIUS - -SHRUB_RADIUS) + 1))));
+				l.setZ(z + (-SHRUB_RADIUS + (int) (random.nextDouble() * ((SHRUB_RADIUS - -SHRUB_RADIUS) + 1))));
+				l.setY(world.getHighestBlockYAt(l.getBlockX(), l.getBlockZ()));
+				shrubLoc[i] = l;
 			}
 			int treeType = random.nextInt(100);
-			
-			if (treeType < 15) {
-				world.generateTree(new Location(world, x, y, z), TreeType.BIRCH);
+			boolean treePlanted = false;
+
+			if (treeType < birchChance) {
+				treePlanted = world.generateTree(new Location(world, x, y, z), TreeType.BIRCH);
+			} else if (treeType < redwoodChance) {
+				treePlanted = world.generateTree(new Location(world, x, y, z), TreeType.REDWOOD);
+			} else if (treeType < tallRedwoodChance) {
+				treePlanted = world.generateTree(new Location(world, x, y, z), TreeType.TALL_REDWOOD);
+			} else if (treeType < treeChance) {
+				treePlanted = world.generateTree(new Location(world, x, y, z), TreeType.TREE);
+			} else if (treeType < bigTreeChance) {
+				treePlanted = world.generateTree(new Location(world, x, y, z), TreeType.BIG_TREE);
 			}
-			else if (treeType < 30) {
-				world.generateTree(new Location(world, x, y, z), TreeType.TREE);
-			}
-			else {
-				world.generateTree(new Location(world, x, y, z), TreeType.BIG_TREE);
+			if (treePlanted) {
+				for (int i = 0; i < shrubNum; i++) {
+					Location l = shrubLoc[i];
+					Block b = world.getBlockAt(l.getBlockX(), world.getHighestBlockYAt(l.getBlockX(), l.getBlockZ()),
+							l.getBlockZ());
+					if (l.getBlockX() != x && l.getBlockZ() != z && b.getRelative(0, -1, 0).getType() == Material.GRASS) {
+						b.setType(Material.LONG_GRASS);
+
+						b.setData((byte) (random.nextInt(100) < 3 ? 0 : 2));
+					}
+				}
 			}
 		}
 	}
+
 }
