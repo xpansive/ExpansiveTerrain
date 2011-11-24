c09132424a1f418d4397f75c5faefd42a6a24171
xpansive <xpansive@live.ca>
2011-11-23 18:52:25 -0700

    Make pumpkin populator take arguments and add comments

--- src/main/java/com/xpansive/bukkit/expansiveterrain/populators/PumpkinPopulator.java
+++ src/main/java/com/xpansive/bukkit/expansiveterrain/populators/PumpkinPopulator.java
@@ -5,19 +5,35 @@ import java.util.Random;
 import org.bukkit.Chunk;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.generator.BlockPopulator;
 
-public class PumpkinPopulator extends RandomDistributionPopulator {
-    public PumpkinPopulator() {
-        NUM_BLOCKS = 20;
-        SIZE = 16;
-        CHANCE_PER_1000 = 5;
-        BLOCK_TYPE = Material.PUMPKIN;
-        BLOCK_DATA = 5;
-        UNDER_BLOCK_TYPE = Material.GRASS;
+public class PumpkinPopulator extends BlockPopulator {
+
+    int chancePer1000, numPumpkins, depositRadius;
+
+    public PumpkinPopulator(int chancePer1000, int numPumpkins, int depositRadius) {
+        this.chancePer1000 = chancePer1000;
+        this.numPumpkins = numPumpkins;
+        this.depositRadius = depositRadius;
     }
 
     @Override
     public void populate(World world, Random random, Chunk source) {
-        randomDistribution(world, random, source);
+        // Check if we should place a pumpkin patch on this chunk
+        if (random.nextInt(1000) < chancePer1000) {
+            int x = (source.getX() << 4) + random.nextInt(16);
+            int z = (source.getZ() << 4) + random.nextInt(16);
+
+            for (int i = 0; i < random.nextInt(numPumpkins); i++) {
+                // Pick a random spot within the radius
+                int cx = x + random.nextInt(depositRadius * 2) - depositRadius;
+                int cz = z + random.nextInt(depositRadius * 2) - depositRadius;
+                int y = world.getHighestBlockYAt(cx, cz);
+
+                // If it's grass, place a pumpkin on top
+                if (world.getBlockAt(cx, y - 1, cz).getType() == Material.GRASS)
+                    world.getBlockAt(cx, y, cz).setType(Material.PUMPKIN);
+            }
+        }
     }
 }
