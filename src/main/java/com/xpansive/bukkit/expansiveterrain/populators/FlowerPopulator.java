db68169f33c7a87c7a49882222330e7da8352b6a
xpansive <xpansive@live.ca>
2011-11-23 18:52:54 -0700

    Make flower populator take arguments and add comments

--- src/main/java/com/xpansive/bukkit/expansiveterrain/populators/FlowerPopulator.java
+++ src/main/java/com/xpansive/bukkit/expansiveterrain/populators/FlowerPopulator.java
@@ -4,32 +4,44 @@ import java.util.Random;
 import org.bukkit.Chunk;
 import org.bukkit.Material;
 import org.bukkit.World;
-import org.bukkit.block.Biome;
 import org.bukkit.generator.BlockPopulator;
 
 public class FlowerPopulator extends BlockPopulator {
 
-    private final int NUM_STEPS = 10;
+    private int flowerPatchChance, plantFlowerChance, numSteps;
+    private Material[] flowers;
+
+    public FlowerPopulator(int flowerPatchChance, int plantFlowerChance,
+            Material[] flowers, int numSteps) {
+        this.flowerPatchChance = flowerPatchChance;
+        this.plantFlowerChance = plantFlowerChance;
+        this.flowers = flowers;
+        this.numSteps = numSteps;
+    }
 
     @Override
     public void populate(World world, Random random, Chunk source) {
-        if (random.nextInt(100) < 7) { // 7% chance
+        // Check if we should plant a flower patch here
+        if (random.nextInt(100) < flowerPatchChance) {
             int x = (source.getX() << 4) + random.nextInt(16);
             int z = (source.getZ() << 4) + random.nextInt(16);
-            Material flowerType = random.nextBoolean() ? Material.RED_ROSE : Material.YELLOW_FLOWER;
 
-            for (int i = 0; i < NUM_STEPS; i++) {
+            // Retrieve a random block type from the list of flower types
+            // The flowers array can be weighted if the generator wants it
+            Material flowerType = flowers[random.nextInt(flowers.length)];
+
+            for (int i = 0; i < numSteps; i++) {
+                // Move both the x and z by a random amount between -1 and 1
                 x += random.nextInt(3) - 1;
                 z += random.nextInt(3) - 1;
                 int y = world.getHighestBlockYAt(x, z);
-                if (world.getBlockAt(x, y - 1, z).getType() == Material.GRASS && world.getBlockAt(x, y - 1, z).getBiome() != Biome.DESERT && random.nextInt(100) > 5) {
-                    setBlock(world, x, y, z, flowerType);
+
+                // Make sure we're planting it on grass
+                if (world.getBlockAt(x, y - 1, z).getType() == Material.GRASS
+                        && random.nextInt(100) < plantFlowerChance) {
+                    world.getBlockAt(x, y, z).setType(flowerType);
                 }
             }
         }
-    }
-
-    private static void setBlock(World w, int x, int y, int z, Material m) {
-        w.getBlockAt(x, y, z).setType(m);
     }
 }
