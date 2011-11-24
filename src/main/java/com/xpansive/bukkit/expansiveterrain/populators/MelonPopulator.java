6fb69ad66252323ee4323b4895bca360e61f45fd
xpansive <xpansive@live.ca>
2011-11-23 18:52:35 -0700

    Make melon populator take arguments and add comments

--- src/main/java/com/xpansive/bukkit/expansiveterrain/populators/MelonPopulator.java
+++ src/main/java/com/xpansive/bukkit/expansiveterrain/populators/MelonPopulator.java
@@ -5,19 +5,35 @@ import java.util.Random;
 import org.bukkit.Chunk;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.generator.BlockPopulator;
 
-public class MelonPopulator extends RandomDistributionPopulator {
-    public MelonPopulator() {
-        NUM_BLOCKS = 20;
-        SIZE = 16;
-        CHANCE_PER_1000 = 5;
-        BLOCK_TYPE = Material.MELON_BLOCK;
-        BLOCK_DATA = 0;
-        UNDER_BLOCK_TYPE = Material.GRASS;
+public class MelonPopulator extends BlockPopulator {
+
+    int chancePer1000, numMelons, depositRadius;
+
+    public MelonPopulator(int chancePer1000, int numMelons, int depositRadius) {
+        this.chancePer1000 = chancePer1000;
+        this.numMelons = numMelons;
+        this.depositRadius = depositRadius;
     }
 
     @Override
     public void populate(World world, Random random, Chunk source) {
-        randomDistribution(world, random, source);
+        // Check if we should place a melon patch on this chunk
+        if (random.nextInt(1000) < chancePer1000) {
+            int x = (source.getX() << 4) + random.nextInt(16);
+            int z = (source.getZ() << 4) + random.nextInt(16);
+
+            for (int i = 0; i < random.nextInt(numMelons); i++) {
+                // Pick a random spot within the radius
+                int cx = x + random.nextInt(depositRadius * 2) - depositRadius;
+                int cz = z + random.nextInt(depositRadius * 2) - depositRadius;
+                int y = world.getHighestBlockYAt(cx, cz);
+
+                // If it's grass, place a melon on top
+                if (world.getBlockAt(cx, y - 1, cz).getType() == Material.GRASS)
+                    world.getBlockAt(cx, y, cz).setType(Material.MELON_BLOCK);
+            }
+        }
     }
 }
