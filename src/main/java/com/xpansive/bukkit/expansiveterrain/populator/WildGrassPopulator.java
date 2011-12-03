4524dfbfed5ec0b0c2bf417e390b531def33c3d0
xpansive <xpansive@live.ca>
2011-12-02 19:32:02 -0700

    Wild grass populator fixups

--- src/main/java/com/xpansive/bukkit/expansiveterrain/populator/WildGrassPopulator.java
+++ src/main/java/com/xpansive/bukkit/expansiveterrain/populator/WildGrassPopulator.java
@@ -10,42 +10,32 @@ import org.bukkit.generator.BlockPopulator;
 
 public class WildGrassPopulator extends BlockPopulator {
 
+    private int minSteps, maxSteps;
+
+    public WildGrassPopulator(int minSteps, int maxSteps) {
+        this.minSteps = minSteps;
+        this.maxSteps = maxSteps;
+    }
+
     @Override
     public void populate(World world, Random random, Chunk source) {
         int x = (source.getX() << 4);
         int z = (source.getZ() << 4);
         int y = world.getHighestBlockYAt(x, z);
-        int numSteps = 0;
 
-        switch (world.getBlockAt(x, y, z).getBiome()) {
-        case RAINFOREST:
-            numSteps = 15;
-            break;
-        case SEASONAL_FOREST:
-            numSteps = 5;
-            break;
-        case FOREST:
-            numSteps = 10;
-            break;
-        case TAIGA:
-            numSteps = 5;
-            break;
-        case PLAINS:
-            numSteps = 50;
-            break;
-        }
+        // Determine the size/steps
+        int numSteps = random.nextInt(maxSteps - minSteps + 1) + minSteps;
 
-        numSteps = random.nextInt(numSteps + 1);
-
+        // Random walking
         for (int i = 0; i < numSteps; i++) {
             x += random.nextInt(3) - 1;
             z += random.nextInt(3) - 1;
             y = world.getHighestBlockYAt(x, z);
             Block b = world.getBlockAt(x, y, z);
 
-            if (b.getRelative(0, -1, 0).getType() == Material.GRASS) {
+            if (b.getRelative(0, -1, 0).getType() == Material.GRASS && b.getTypeId() == 0) {
                 b.setType(Material.LONG_GRASS);
-                b.setData((byte) 1);
+                b.setData((byte) 1); // The default wild grass type
             }
         }
     }
