356433224fe83e459cafac39b767797b03c8dcd0
xpansive <xpansive@live.ca>
2011-12-05 16:06:46 -0700

    Some sensible changes to LargeStructureGenerator

--- src/main/java/com/xpansive/bukkit/expansiveterrain/structure/LargeStructureGenerator.java
+++ src/main/java/com/xpansive/bukkit/expansiveterrain/structure/LargeStructureGenerator.java
@@ -8,12 +8,12 @@ import org.bukkit.World;
 public abstract class LargeStructureGenerator {
     public abstract void fillColumn(World world, Random rand, int worldX, int worldZ, int x, int z, byte[] chunkData);
 
-    public static void setBlock(byte[] data, int x, int y, int z, Material type) {
+    protected void setBlock(byte[] data, int x, int y, int z, Material type) {
         if (x < 16 && x >= 0 && z < 16 && z >= 0 && y < 128 && y >= 0)
             data[(x * 16 + z) * 128 + y] = (byte) type.getId();
     }
     
-    public static Material getBlock(byte[] data, int x, int y, int z) {
+    protected Material getBlock(byte[] data, int x, int y, int z) {
         if (x < 16 && x >= 0 && z < 16 && z >= 0 && y < 128 && y >= 0)
             return Material.getMaterial(data[(x * 16 + z) * 128 + y]);
         return Material.AIR;
